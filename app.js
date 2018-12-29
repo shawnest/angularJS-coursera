18a10bdab5f1ec0afc691863bb52cf5efc7548c3
Akos <herkoak@gmail.com>
2018-12-29 11:20:38 +0100

    First assignment bonus

--- app.js
+++ app.js
@@ -8,20 +8,24 @@
         $scope.dishes = "";
         $scope.name = "Yaakov";
         $scope.message = "";
+        $scope.messageStyle = "";
 
         $scope.displayMessage = function(){
             var items = calculateItemsNo($scope.dishes);
             if($scope.dishes == ""){
                 $scope.message = "Please enter data first";
+                $scope.messageStyle = {"color": "red", "border" : "1px solid red"};
             }else if(items < 4){
                 $scope.message = "Enjoy";
+                $scope.messageStyle = {"color": "green", "border" : "1px solid green"};
             }else if(items >3){
                 $scope.message = "Too much!";
+                $scope.messageStyle = {"color": "green", "border" : "1px solid green"};
             }
         };
 
         function calculateItemsNo(string){
-            var words = string.split(',');
+            var words = string.split(',').filter(function(el) {return el.length != 0})
             return words.length;
         }
     }
